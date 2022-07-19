1f2bb6e6f06a4b30d63a43557ba2166ed7937813
anreh <anurehka@gmail.com>
2022-07-19 10:32:03 +0800

    Commit 20220719 1029 - Added user interactivity - EditText added - Update button added

--- app/src/main/java/com/example/practical2/MainActivity.kt
+++ app/src/main/java/com/example/practical2/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.example.practical2
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImg: ImageView
     lateinit var numberText: TextView
+    lateinit var editPlayerName: EditText
+    lateinit var playerNameTv: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         diceImg = findViewById(R.id.diceImg)
         numberText = findViewById(R.id.numberText)
+        editPlayerName = findViewById(R.id.editPlayerName)
+        playerNameTv = findViewById(R.id.playerName)
 
         //val button: Button = findViewById(R.id.rollButton)
         val button: Button = findViewById<Button>(R.id.rollButton)
         button.setOnClickListener { rollDice() }
+        val updateButton = findViewById<Button>(R.id.updatePlayerNameBtn)
+        updateButton.setOnClickListener { updatePlayerName(it) }
     }
 
     private fun rollDice() {
@@ -38,5 +44,14 @@ class MainActivity : AppCompatActivity() {
         diceImg.setImageResource(imgSrc)
 
         Toast.makeText(this, randomNumb.toString(), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun updatePlayerName(view: View){
+        playerNameTv.text = editPlayerName.text
+
+        //Hide Keyboard after update player name
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
     }
 }
